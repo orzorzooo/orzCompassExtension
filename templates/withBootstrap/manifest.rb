4f4b791d7cfab3b28975b54b4042f94bc26564b3
apdcOrz <apdcOrz@apdcOrzde-MacBook-Pro.local>
2016-02-25 18:28:54 +0800

    mv bootstrap

---
+++ templates/withBootstrap/manifest.rb
@@ -0,0 +1,17 @@
+description "Susy: custom Sass layouts. Your markup, your design, our math."
+
+discover :stylesheets
+
+html 'index.html.erb', :erb => true
+
+help %Q{
+Full documentation and tutorials available online:
+
+    http://susy.oddbird.net/
+}
+
+welcome_message %Q{
+Welcome to Susy! Check out the full documentation online:
+
+    http://susy.oddbird.net/
+}
