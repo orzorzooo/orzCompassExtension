a49e9467bfdb350008cfb58658cf83e269380a75
apdcOrz <apdcOrz@apdcOrzde-MacBook-Pro.local>
2016-05-05 15:07:33 +0800

    add jquery

---
+++ templates/bootstrap_jquery/data.js
@@ -0,0 +1,36 @@
+testData = [{
+  "id": 01,
+  "name": "gary",
+  "gender": "male",
+  "point": 30,
+  "point_content": [
+    {"amount": 50 },
+    {"money": 30}
+  ]
+},
+{
+  "id": 02,
+  "name": "jones",
+  "gender": "female"
+},
+{
+  "id": 02,
+  "name": "jones",
+  "gender": "female"
+},
+{
+  "id": 02,
+  "name": "jones",
+  "gender": "female"
+},
+{
+  "id": 02,
+  "name": "jones",
+  "gender": "female"
+},
+{
+  "id": 02,
+  "name": "jones",
+  "gender": "female"
+},
+];
---
+++ templates/bootstrap_jquery/manifest.rb
@@ -0,0 +1,16 @@
+description "modify by orzorzooo"
+
+discover :stylesheets
+discover :javascripts
+
+html 'index.html.erb', :erb => true
+
+help %Q{
+Full documentation and tutorials available online:
+
+    http://susy.oddbird.net/
+}
+
+welcome_message %Q{
+Welcome orzorzooo!
+}
